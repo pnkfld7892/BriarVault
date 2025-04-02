b7c19385656921f557c3830bae4f17a57c40ee71
Eli Schwarz <eschwarz0@gmail.com>
2025-04-01 22:15:58 -0400

    Svelte init and basic nav

---
+++ BriarVault.Svelte/src/lib/components/Nav.svelte
@@ -0,0 +1,11 @@
+<script>
+	import BriarVaultLogo from './svg/BriarVaultLogo.svelte';
+	import Github from './svg/Github.svelte';
+</script>
+
+<nav class="mx-auto flex h-24 items-center justify-between px-4">
+	<a href="/"> <BriarVaultLogo class='w-24 h-24 rounded-4xl'></BriarVaultLogo></a>
+	<div class="float-end">
+		<a href="https://github.com/pnkfld7892/BriarVault" target="_blank"><Github class="w-14 h-14"></Github></a>
+	</div>
+</nav>
---
+++ BriarVault.Svelte/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import '../app.css';
+	import Nav from '../lib/components/Nav.svelte';
+
+	let { children } = $props();
+</script>
+
+<header class="fixed inset-x-0 top-0 z-0 border-b">
+	<Nav></Nav>
+</header>
+<main>
+	<slot />
+</main>
